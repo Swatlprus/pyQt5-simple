31f39e17760a191bf0c5c4c73ed8a5940e556f5c
Ринат <swatlprus@gmail.com>
2021-03-01 18:50:29 +0500

    Добавил модуль по разделению PDF

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import os
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+import os.path
+
+
+from requests import post
+import json
+import argparse
+import base64
+import re
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 
 
 class Form(QMainWindow):
@@ -12,14 +22,18 @@ class Form(QMainWindow):
         self.plainTextEdit = QPlainTextEdit()
         self.plainTextEdit.setFont(QFont('Arial', 11))
 
-        openDirButton = QPushButton("Открыть каталог:")
+        openDirButton = QPushButton("Выбрать каталог:")
         openDirButton.clicked.connect(self.getDirectory)
 
+        openFileButton = QPushButton("Выбрать файл:")
+        openFileButton.clicked.connect(self.getFilename)
+
         startButton = QPushButton("Запустить")
-        startButton.clicked.connect(self.getDirectory)
+        startButton.clicked.connect(self.startSlice)
 
         layoutV = QVBoxLayout()
         layoutV.addWidget(openDirButton)
+        layoutV.addWidget(openFileButton)
         layoutV.addWidget(startButton)
 
         layoutH = QHBoxLayout()
@@ -33,9 +47,29 @@ class Form(QMainWindow):
         self.resize(600, 50)
         self.setWindowTitle("Разделение PDF-документа")
 
-    def getDirectory(self):  # <-----
+    def getDirectory(self):
+        global dirlist
         dirlist = QFileDialog.getExistingDirectory(self, "Выбрать папку", ".")
-        self.plainTextEdit.appendHtml("<br><b>{}</b>".format(dirlist))
+        self.plainTextEdit.appendHtml("Каталог: <b>{}</b>".format(dirlist))
+
+    def getFilename(self):
+        global filename
+        filename, filetype = QFileDialog.getOpenFileName(self, "Выбрать файл", ".")
+        self.plainTextEdit.appendHtml("Файл: <b>{}</b>".format(filename, filetype))
+
+    def startSlice(self):
+        # Модуль по разделению PDF на страницы - ЧАСТЬ 1
+        pdf_document = filename
+        pdf = PdfFileReader(pdf_document)
+        for page in range(pdf.getNumPages()):
+            pdf_writer = PdfFileWriter()
+            current_page = pdf.getPage(page)
+            pdf_writer.addPage(current_page)
+            print(os.path.exists(dirlist + '/Slice'))
+            outputFilename = "C:/Users/Ринат/PycharmProjects/pdfSlice/Slice/Slice-page-{}.pdf".format(page + 1)
+            with open(outputFilename, "wb") as out:
+                pdf_writer.write(out)
+                print("created", outputFilename)
 
 
 if __name__ == '__main__':
